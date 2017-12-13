89c89e306ab6736d58f87778be92073db429e329
itzt <wskaitt@sina.cn>
2017-12-13 16:09:17 +0800

    新增了en语言文件。

---
+++ resources/lang/en/category.php
@@ -0,0 +1,28 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    | The article language file management
+    |
+    */
+
+return [
+    // Add a list
+    'add_title'         => 'Column setup',
+    'add_prev'          => 'Prev column',
+    'add_top'           => 'Top category',
+    'add_cat_name'      => 'Category name',
+    'add_desc'          => 'Describe',
+    'add_cat_desc'      => 'Say something...Can be ignored',
+
+    // Category a list
+    'show_title'        => 'Category column',
+    'show_break'        => 'Column management',
+    'show_add_category' => 'Add columns',
+    'show_list_catname' => 'Column name',
+    
+
+
+];
---
+++ resources/lang/en/navigate.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    | Navigation management language files
+    |
+    */
+
+    'update'        =>  'Navigation to edit',
+
+    'nav_name'      =>  'The name of the navigation',
+    'jump_url'      =>  'Skip links',
+    'is_new_open'   =>  'Whether the new TAB',
+    'parent_nav'    =>  'The higher the navigation',
+    'top_nav'       =>  'Top navigation',
+    'nav_create'    =>  'Add navigation',
+    'sort'          =>  'The sorting'
+    
+
+];
